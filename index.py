8123dd135c970c5ec6c332c7ccf7f1370edf2d0b
engj <justin@ubuntu.(none)>
2013-10-17 15:28:14 -0700

    moved model code to index.py

--- index.py
+++ index.py
@@ -3,12 +3,21 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 from google.appengine.ext.webapp import template
 import logging
+​from google.appengine.ext import ndb
 
 class MainPage(webapp.RequestHandler):
   def get(self):
     template_values={}
     path=os.path.join(os.path.dirname(__file__),'index.html')
     self.response.out.write(template.render(path,template_values))
+
+class Mosay(ndb.Model){
+    currentIndex = ndb.IntegerProperty()
+
+class Picture(ndb.Model):
+    url = ndb.StringProperty()
+    modulus = ndb.IntegerProperty()
+
 
 application=webapp.WSGIApplication(
                                     [('/', MainPage)],
--- model.py
+++
@@ -1,9 +0,0 @@
-​from google.appengine.ext import ndb
-
-class Mosay(ndb.Model){
-    currentIndex = ndb.IntegerProperty()
-
-class Picture(ndb.Model):
-    url = ndb.StringProperty()
-    modulus = ndb.IntegerProperty()
-
