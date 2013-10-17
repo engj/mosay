624d7022a9ea715b09fb878d4b2f7c7a18bc37ca
engj <justin@ubuntu.(none)>
2013-10-17 15:31:52 -0700

    fixed syntax errors

--- index.py
+++ index.py
@@ -1,17 +1,18 @@
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, ndb
 from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 from google.appengine.ext.webapp import template
 import logging
-​from google.appengine.ext import ndb
 
 class MainPage(webapp.RequestHandler):
   def get(self):
+    picture = Picture(url="http://www.google.com/", modulus=32)
+    picture.put()
     template_values={}
     path=os.path.join(os.path.dirname(__file__),'index.html')
     self.response.out.write(template.render(path,template_values))
 
-class Mosay(ndb.Model){
+class Mosay(ndb.Model):
     currentIndex = ndb.IntegerProperty()
 
 class Picture(ndb.Model):
